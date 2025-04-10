09deb7cbc7c3345f95923f44ac02d7e023704d6e
vamsee1317 <vamsee.achieversit@gmail.com>
2025-04-10 14:24:11 +0530

    V.0.0.1

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Notifications from "./components/Notification/Notifications";
+
+function App(){
+
+  const notifications = [
+    {
+      type: "info",
+      message: "Your profile has been updated successfully.",
+      timestamp: "2025-04-10T10:30:00Z",
+      isRead: false
+    },
+    {
+      type: "warning",
+      message: "Your subscription will expire in 3 days.",
+      timestamp: "2025-04-10T09:15:00Z",
+      isRead: false
+    },
+    {
+      type: "error",
+      message: "Failed to load data from the server.",
+      timestamp: "2025-04-09T22:45:00Z",
+      isRead: true
+    },
+    {
+      type: "error",
+      message: "Failed to load data from the server 404 occured.",
+      timestamp: "2025-04-10T20:10:00Z",
+      isRead: true
+    },
+
+    {
+      type: "success",
+      message: "Failed to load data from the server 404 occured.",
+      timestamp: "2025-04-10T20:10:00Z",
+      isRead: true
+    }
+  ];
+  
+
+
+  return(
+    <div>
+        {
+          notifications.map((note, i)=>(
+            <Notifications key={i} {...note}/>
+          ))
+        }
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Notification/Notifications.jsx
@@ -0,0 +1,51 @@
+// import React, { Component } from "react";
+
+// class Notifications extends Component{
+//     constructor(){
+//         super();
+//     }
+
+//     render(){
+//         return(
+//             <div>
+//             </div>
+//         )
+//     }
+// }
+
+
+import React from "react"
+
+function Notifications({type, message, timestamp, isRead}){
+    console.log(type, message, timestamp, isRead);
+
+    // let bgColor;
+
+    // if(type == "info"){
+    //     bgColor = "bg-blue-100"
+    // }else if(type == "warning"){
+    //     bgColor = "bg-yellow-100"
+    // }else if(type == "error"){
+    //     bgColor = "bg-red-100"
+    // }else{
+    //     bgColor = "bg-green-100"
+    // }
+
+    const bgColor = {
+        "info": "bg-blue-100",
+        "warning": "bg-yellow-100",
+        "error": "bg-red-100",
+        "success": "bg-green-500"
+    }[type];
+
+
+
+    return(
+        <div className={`p-4 rounded shadow-sm mb-3 ${bgColor} ${isRead ? 'opacity-50': ''} : `}>
+            <p className="font-semibold">{message}</p>
+            <p className="text-gray-500">{timestamp}</p>
+        </div>
+    )
+}
+
+export default Notifications
